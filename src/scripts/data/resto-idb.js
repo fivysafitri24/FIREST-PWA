34a249c408dfc08f0644d5a2333d20af666db6c5
Fivysafitri24 <89623457+fivysafitri24@users.noreply.github.com>
2022-12-05 11:44:45 +0700

    Create resto-idb.js

---
+++ src/scripts/data/resto-idb.js
@@ -0,0 +1,33 @@
+import { openDB } from 'idb';
+import CONFIG from '../globals/config';
+
+const { DB_NAME, DB_VERSION, OBJECT_STORE_NAME } = CONFIG;
+
+const openIdb = openDB(DB_NAME, DB_VERSION, {
+  upgrade(db) {
+    db.createObjectStore(OBJECT_STORE_NAME, {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+  },
+});
+
+const FavRestoIdb = {
+  async getResto(id) {
+    return (await openIdb).get(OBJECT_STORE_NAME, id);
+  },
+
+  async getAllResto() {
+    return (await openIdb).getAll(OBJECT_STORE_NAME);
+  },
+
+  async putResto(resto) {
+    return (await openIdb).put(OBJECT_STORE_NAME, resto);
+  },
+
+  async deleteResto(id) {
+    return (await openIdb).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavRestoIdb;
